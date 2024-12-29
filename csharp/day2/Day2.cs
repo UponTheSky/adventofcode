cc8316406edf17da30835224934b52244ecec64a
UponTheSky <sailing.to.orbis@gmail.com>
2024-12-29 09:26:13 +0900

    csharp: day2 - part2

--- csharp/day2/Day2.cs
+++ csharp/day2/Day2.cs
@@ -1,4 +1,8 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace CSharp.Day2
@@ -8,6 +12,12 @@ namespace CSharp.Day2
         public override int Run()
         {
             IEnumerable<string> lines = ReadFrom(file);
+
+            return PartTwo(lines);
+        }
+
+        private static int PartOne(IEnumerable<string> lines)
+        {
             int safeCount = 0;
 
             foreach (string line in lines)
@@ -57,6 +67,86 @@ namespace CSharp.Day2
             }
 
             return safeCount;
+        }
+
+        private static int PartTwo(IEnumerable<string> lines)
+        {
+            int safeCount = 0;
+
+            foreach (string line in lines)
+            {
+                List<string>? numbers = [.. line.Split(' ')];
+                Debug.Assert(numbers != null && numbers.Count > 2);
+
+                List<int> numbersInt = numbers.Select(Int32.Parse).ToList();
+                int positiveCount = 0;
+                int negativeCount = 0;
+
+                for (int i = 0; i < numbersInt.Count - 1; i++)
+                {
+                    int diff = numbersInt[i + 1] - numbersInt[i];
+                    if (diff > 0)
+                    {
+                        positiveCount++;
+                    }
+                    else if (diff < 0)
+                    {
+                        negativeCount++;
+                    }
+                }
+
+                if (negativeCount < numbersInt.Count - 2 && positiveCount < numbersInt.Count - 2)
+                {
+                    continue;
+                }
+
+                if (negativeCount >= numbersInt.Count - 2)
+                {
+                    numbersInt.Reverse();
+                }
+
+                if (CheckValidArray(numbersInt))
+                {
+                    safeCount++;
+                }
+            }
+
+            return safeCount;
+        }
+
+        private static bool CheckValidArray(List<int> numbers)
+        {
+            if (BruteForce(numbers))
+            {
+                return true;
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<int> newNumbers = numbers.Where((_, index) => index != i).ToList();
+
+                if (BruteForce(newNumbers))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool BruteForce(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                int diff = numbers[i + 1] - numbers[i];
+
+                if (diff <= 0 || diff > 3)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
